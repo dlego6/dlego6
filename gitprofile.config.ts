e65a19df3664c5e9a8d94dfdf244532ef8a39600
Diego S. <168074656+dlego6@users.noreply.github.com>
2024-11-10 18:14:22 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      header: 'Mis proyectos',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
